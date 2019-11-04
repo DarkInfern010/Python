25b4c5b208db060555b43fdb7ff5a6707202ab0d
DarkInfern010 <darkinfern010@gmail.com>
2019-11-04 18:13:45 +0100

    Add struc exo 7

--- Cnam-Structure/Tableau/exo7.py
+++ Cnam-Structure/Tableau/exo7.py
@@ -25,8 +25,47 @@ def PlusLongueCoupeIte(tab):
     return indice[indiceMaxRes]
 #enddef
 
-n = int(input("Entrer la taille du tableau : "))
-tab = [random.randint(0,1) for i in range(n)]
+def PlusLongueCoupeRec(tab, nbO):
+    pivot = round(len(tab)/2)
+    foundi = False
+    foundj = False
+    i = 0
+    j = 0
+    indiceDeb = 0
+    indiceFin = 0
+
+    while(foundi == False and foundj == False):
+        i += 1
+        j += 1
+
+        if(tab[pivot - i] == 1):
+            indiceDeb = len(tab) - pivot - i
+        else:
+            foundi = True
+
+        if (tab[pivot + j] == 1):
+            indiceFin = len(tab) - pivot + j
+        else:
+            foundj = True
+        #endif
+    #endwhile
+    nbO.append(indiceFin-indiceDeb)
+
+    if (len(tab[:indiceDeb]) > 2):
+        PlusLongueCoupeRec(tab[:indiceDeb],nbO)
+    if (len(tab[indiceFin+1:]) > 2):
+        PlusLongueCoupeRec(tab[indiceFin+1:],nbO)
+    if (len(tab[indiceFin+1:]) == 1 or len(tab[:indiceDeb]) == 1):
+        nbO.append(1)
+    return nbO
+#endef
+
+#n = int(input("Entrer la taille du tableau : "))
+#tab = [random.randint(0,1) for i in range(n)]
+tab = [0,1,1,1,0,1,0,1,1,1,1,1,1,1,1,0,0,0,1,1]
+nbO = []
+
 print(tab)
 
-print (PlusLongueCoupeIte(tab))
+#print (PlusLongueCoupeIte(tab))
+print (PlusLongueCoupeRec(tab,nbO))
