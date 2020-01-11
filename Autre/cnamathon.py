79a2331484fd76c489b1c70af83602b42f5a0846
DarkInfern010 <darkinfern010@gmail.com>
2020-01-11 16:21:54 +0100

    Pydefis

--- Autre/cnamathon.py
+++ Autre/cnamathon.py
@@ -1,20 +1,23 @@
-import win32api, win32con, time
+from pynput.mouse import Button, Controller as MouseController
+import time
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-#1-Star (734, 524) 5-Star(823,524)
+mouse = MouseController()
+sleep = 0.2
+
+def click(x, y):
+    mouse.position = (x, y)
+    mouse.click(Button.left, 1)
+    time.sleep(sleep)
+#enddef
+
+print ("Current position: " + str(mouse.position))
+
 for i in range (50):
-    click(1495,54)
-    time.sleep(0.5)
-    click(1275,423)
-    time.sleep(0.5)
-    click(823, 524)
-    time.sleep(0.5)
-    click(1495,54)
-    time.sleep(0.5)
-    click(1307,482)
-    time.sleep(0.5)
-    click(89, 47)
-    time.sleep(3)
+    click(-1137, 67)
+    click(-1346, 447)
+    click(-1056, 564)
+    click(-1407, 507)
+    click(-1137, 67)
+    click(-1342, 490)
+    click(-1832, 63)
+    print(i)
---
+++ Pydefis/Herculito10.py
@@ -0,0 +1,5 @@
+# Blanc <
+# Roux =
+# Noir >
+# B*N*R = (B+N+R)*777
+
